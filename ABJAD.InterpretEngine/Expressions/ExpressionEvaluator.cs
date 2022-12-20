37b075480287333d33901149b07cac772576f51e
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2022-12-20 17:00:03 +0200

    Finish with interpreting expressions

--- ABJAD.InterpretEngine/Expressions/ExpressionEvaluator.cs
+++ ABJAD.InterpretEngine/Expressions/ExpressionEvaluator.cs
@@ -1,11 +1,29 @@
 ﻿using ABJAD.InterpretEngine.Shared.Expressions;
+using ABJAD.InterpretEngine.Shared.Expressions.Assignments;
+using ABJAD.InterpretEngine.Shared.Expressions.Binary;
+using ABJAD.InterpretEngine.Shared.Expressions.Fixes;
+using ABJAD.InterpretEngine.Shared.Expressions.Unary;
 
-namespace ABJAD.InterpretEngine;
+namespace ABJAD.InterpretEngine.Expressions;
 
 public class ExpressionEvaluator : Evaluator<Expression>
 {
+    private readonly IExpressionStrategyFactory expressionStrategyFactory;
+
+    public ExpressionEvaluator(IExpressionStrategyFactory expressionStrategyFactory)
+    {
+        this.expressionStrategyFactory = expressionStrategyFactory;
+    }
+
     public EvaluatedResult Evaluate(Expression target)
     {
-        throw new NotImplementedException();
+        return target switch
+        {
+            AssignmentExpression expression => expressionStrategyFactory.GetAssignmentInterpretingStrategy(expression, this).Apply(),
+            BinaryExpression expression => expressionStrategyFactory.GetBinaryExpressionInterpretingStrategy(expression, this).Apply(),
+            FixExpression expression => expressionStrategyFactory.GetFixesInterpretingStrategy(expression).Apply(),
+            UnaryExpression expression => expressionStrategyFactory.GetUnaryExpressionInterpretingStrategy(expression, this).Apply(),
+            _ => throw new ArgumentException()
+        };
     }
 }
