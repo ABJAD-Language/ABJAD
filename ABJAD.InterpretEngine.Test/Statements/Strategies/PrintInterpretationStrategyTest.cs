a6ec22935d02531d4c61078e81a74a421eb8c154
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2022-12-22 19:25:35 +0200

    Added interpretation for print statements

---
+++ ABJAD.InterpretEngine.Test/Statements/Strategies/PrintInterpretationStrategyTest.cs
@@ -0,0 +1,32 @@
+﻿using ABJAD.InterpretEngine.Shared.Expressions;
+using ABJAD.InterpretEngine.Shared.Statements;
+using ABJAD.InterpretEngine.Statements.Strategies;
+using NSubstitute;
+
+namespace ABJAD.InterpretEngine.Test.Statements.Strategies;
+
+public class PrintInterpretationStrategyTest
+{
+    private readonly TextWriter textWriter = Substitute.For<TextWriter>();
+    private readonly Evaluator<Expression> expressionEvaluator = Substitute.For<Evaluator<Expression>>();
+
+    [Fact(DisplayName = "throws error when target has undefined value")]
+    public void throws_error_when_target_has_undefined_value()
+    {
+        var print = new Print() { Target = Substitute.For<Expression>() };
+        expressionEvaluator.Evaluate(print.Target).Returns(new EvaluatedResult { Value = SpecialValues.UNDEFINED });
+        var strategy = new PrintInterpretationStrategy(print, textWriter, expressionEvaluator);
+        Assert.Throws<OperationOnUndefinedValueException>(() => strategy.Apply());
+    }
+
+    [Fact(DisplayName = "writes the value to the text writer")]
+    public void writes_the_value_to_the_text_writer()
+    {
+        var print = new Print() { Target = Substitute.For<Expression>() };
+        var value = new object();
+        expressionEvaluator.Evaluate(print.Target).Returns(new EvaluatedResult { Value = value });
+        var strategy = new PrintInterpretationStrategy(print, textWriter, expressionEvaluator);
+        strategy.Apply();
+        textWriter.Received(1).WriteLine(value);
+    }
+}
