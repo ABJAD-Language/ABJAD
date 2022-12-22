1ca6d64a04df7b1df70408cc0fc17f48429496f9
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2022-12-22 17:16:22 +0200

    Added interpretation for if-else statements

---
+++ ABJAD.InterpretEngine/Shared/Statements/Conditional.cs
@@ -0,0 +1,9 @@
+﻿using ABJAD.InterpretEngine.Shared.Expressions;
+
+namespace ABJAD.InterpretEngine.Shared.Statements;
+
+public class Conditional
+{
+    public Expression Condition { get; set; }
+    public Statement Body { get; set; }
+}
---
+++ ABJAD.InterpretEngine/Shared/Statements/IfElse.cs
@@ -0,0 +1,8 @@
+﻿namespace ABJAD.InterpretEngine.Shared.Statements;
+
+public class IfElse
+{
+    public Conditional MainConditional { get; set; }
+    public List<Conditional> OtherConditionals { get; set; }
+    public Statement ElseBody { get; set; }
+}
