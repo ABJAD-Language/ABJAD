67106a4f98b356d26b2b5c6d03389fa7f41bc8d1
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2022-12-25 19:06:19 +0200

    Added interpretation for instance fields access expressions

---
+++ ABJAD.InterpretEngine/Expressions/NotInstanceReferenceException.cs
@@ -0,0 +1,19 @@
+﻿namespace ABJAD.InterpretEngine.Expressions;
+
+public class NotInstanceReferenceException : InterpretationException
+{
+    public NotInstanceReferenceException(string reference) 
+        : base(FormulateArabicMessage(reference), FormulateEnglishMessage(reference))
+    {
+    }
+
+    private static string FormulateArabicMessage(string reference)
+    {
+        return $"المؤشر {reference} ليس وحدة من صنف معين.";
+    }
+
+    private static string FormulateEnglishMessage(string reference)
+    {
+        return $"Reference {reference} is not an instance of a class.";
+    }
+}
---
+++ ABJAD.InterpretEngine/Shared/Expressions/InstanceFieldAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace ABJAD.InterpretEngine.Shared.Expressions;
+
+public class InstanceFieldAccess : Expression
+{
+    public string Instance { get; set; }
+    public List<string> NestedFields { get; set; } = new();
+}
