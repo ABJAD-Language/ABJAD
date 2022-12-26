b2b849c00a7076cf12448c7e74486425031cfe1b
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2022-12-27 00:20:45 +0200

    Added declaration interpretation integration tests

---
+++ ABJAD.InterpretEngine.Test/Declarations/DeclarationInterpreterIntegrationTest.cs
@@ -0,0 +1,123 @@
+﻿using ABJAD.InterpretEngine.Declarations;
+using ABJAD.InterpretEngine.ScopeManagement;
+using ABJAD.InterpretEngine.Shared;
+using ABJAD.InterpretEngine.Shared.Declarations;
+using ABJAD.InterpretEngine.Shared.Expressions.Primitives;
+using ABJAD.InterpretEngine.Shared.Statements;
+using ABJAD.InterpretEngine.Types;
+using Environment = ABJAD.InterpretEngine.ScopeManagement.Environment;
+
+namespace ABJAD.InterpretEngine.Test.Declarations;
+
+public class DeclarationInterpreterIntegrationTest
+{
+    [Fact(DisplayName = "interpret variable declaration without a value")]
+    public void interpret_variable_declaration_without_a_value()
+    {
+        var environment = new Environment(new List<Scope> { ScopeFactory.NewScope()});
+        var interpreter = new DeclarationInterpreter(environment, new StringWriter());
+
+        var declaration = new VariableDeclaration()
+        {
+            Name = "id",
+            Type = DataType.String()
+        };
+        
+        interpreter.Interpret(declaration);
+        
+        Assert.True(environment.GetReferenceType("id").IsString());
+        Assert.Equal(SpecialValues.UNDEFINED, environment.GetReference("id"));
+        Assert.False(environment.IsReferenceConstant("id"));
+    }
+
+    [Fact(DisplayName = "interpret constant declaration")]
+    public void interpret_constant_declaration()
+    {
+        var environment = new Environment(new List<Scope> { ScopeFactory.NewScope()});
+        var interpreter = new DeclarationInterpreter(environment, new StringWriter());
+
+        var declaration = new ConstantDeclaration()
+        {
+            Name = "id",
+            Type = DataType.Number(),
+            Value = new NumberPrimitive() { Value = 12.0 }
+        };
+        
+        interpreter.Interpret(declaration);
+        
+        Assert.True(environment.GetReferenceType("id").IsNumber());
+        Assert.Equal(12.0, environment.GetReference("id"));
+        Assert.True(environment.IsReferenceConstant("id"));
+    }
+
+    [Fact(DisplayName = "interpret function declaration")]
+    public void interpret_function_declaration()
+    {
+        var environment = new Environment(new List<Scope> { ScopeFactory.NewScope()});
+        var interpreter = new DeclarationInterpreter(environment, new StringWriter());
+
+        var declaration = new FunctionDeclaration()
+        {
+            Name = "func",
+            ReturnType = DataType.Number(),
+            Parameters = new List<Parameter>()
+            {
+                new() { Name = "param", Type = DataType.Custom("type") }
+            },
+            Body = new Block()
+            {
+                Bindings = new List<Binding>()
+                {
+                    new Return() { Target = new NumberPrimitive() { Value = 1.0 }}
+                }
+            }
+        };
+        
+        interpreter.Interpret(declaration);
+        
+        Assert.True(environment.FunctionExists("func", DataType.Custom("type")));
+        Assert.True(environment.GetFunctionReturnType("func", DataType.Custom("type")).IsNumber());
+
+        var functionElement = environment.GetFunction("func", DataType.Custom("type"));
+        Assert.Equal(declaration.Body, functionElement.Body);
+    }
+
+    [Fact(DisplayName = "interpret class declaration")]
+    public void interpret_class_declaration()
+    {
+        var environment = new Environment(new List<Scope> { ScopeFactory.NewScope()});
+        var interpreter = new DeclarationInterpreter(environment, new StringWriter());
+
+        var assignment = new Assignment() { Target = "field", Value = new IdentifierPrimitive() { Value = "_field" }};
+        var declaration = new ClassDeclaration()
+        {
+            Name = "type",
+            Declarations = new List<Declaration>()
+            {
+                new VariableDeclaration() { Name = "field", Type = DataType.String() },
+                new ConstructorDeclaration()
+                {
+                    Parameters = new List<Parameter>()
+                    {
+                        new() { Name = "_field", Type = DataType.String() }
+                    },
+                    Body = new Block()
+                    {
+                        Bindings = new List<Binding>()
+                        {
+                            assignment
+                        }
+                    }
+                }
+            }
+        };
+        
+        interpreter.Interpret(declaration);
+        
+        Assert.True(environment.TypeExists("type"));
+        Assert.True(environment.TypeHasConstructor("type", DataType.String()));
+
+        var classElement = environment.GetType("type");
+        Assert.Equal(assignment, classElement.Constructors[0].Body.Bindings[0]);
+    }
+}
