c44c3c99dc3ec1260a536bfaa287b45c99b2d06f
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2022-12-27 22:55:40 +0200

    Add statement DTOs

---
+++ ABJAD.InterpretEngine.Service/Shared/Statements/ReturnApiModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ABJAD.InterpretEngine.Service.Shared.Statements;
+
+public class ReturnApiModel
+{
+    public object Target { get; }
+
+    public ReturnApiModel(object target)
+    {
+        Target = target;
+    }
+}
