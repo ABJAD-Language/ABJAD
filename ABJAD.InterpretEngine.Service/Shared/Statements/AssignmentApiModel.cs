278a2f3f4df74ab855a3e9efa42efe78de38ffa2
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2022-12-27 22:55:40 +0200

    Add statement DTOs

---
+++ ABJAD.InterpretEngine.Service/Shared/Statements/AssignmentApiModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ABJAD.InterpretEngine.Service.Shared.Statements;
+
+public class AssignmentApiModel
+{
+    public string Target { get; }
+    public object Value { get; }
+
+    public AssignmentApiModel(string target, object value)
+    {
+        Target = target;
+        Value = value;
+    }
+}
---
+++ ABJAD.InterpretEngine.Service/Shared/Statements/BlockApiModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ABJAD.InterpretEngine.Service.Shared.Statements;
+
+public class BlockApiModel
+{
+    public List<object> Bindings { get; }
+
+    public BlockApiModel(List<object> bindings)
+    {
+        Bindings = bindings;
+    }
+}
