4c6fcb3304d5b5dc3b8736a88a29a1c4cf81fea1
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2023-01-07 15:47:19 +0200

    Fix-30: add message in the case of an internal server error

---
+++ ABJAD.InterpretEngine.Service/Api/InternalServerErrorFilterAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ABJAD.InterpretEngine.Service.Api;
+
+public class InternalServerErrorFilterAttribute : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        if (context.Exception is not InterpretationException)
+        {
+            context.Result = new BadRequestObjectResult(new InterpretationFailureException());
+        }
+    }
+}
---
+++ ABJAD.InterpretEngine.Service/Api/InterpretationFailureException.cs
@@ -0,0 +1,19 @@
+﻿namespace ABJAD.InterpretEngine.Service.Api;
+
+public class InterpretationFailureException : InterpretationException
+{
+    public InterpretationFailureException()
+        : base(FormulateArabicMessage(), FormulateEnglishMessage())
+    {
+    }
+
+    private static string FormulateArabicMessage()
+    {
+        return "لقد واجهنا مشكلة خلال محاولة تشغيل الكود. قد يكون هناك خطأ في الكود نفسه، أو عطلٌ من جانبنا.";
+    }
+
+    private static string FormulateEnglishMessage()
+    {
+        return "We have encountered an issue while interpreting your code. It might be a problem from your code, or from our side.";
+    }
+}
